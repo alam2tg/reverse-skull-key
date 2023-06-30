9c1761848afbd7792f62f153c4a44748040dc261
Aaron <aaronlamfinance@gmail.com>
2023-06-30 00:17:54 -0700

    Add all characters to arrays

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,86 @@
 // Assignment code here
-var lowerCase = []
-var upperCase = []
-var numbers = []
+//Create an auto-generated array of numbers
+//check if generated on chrome with console.log
+
+var lowercase = [];
+var uppercase = [];
+var numbers = [];
+var specialChars = [];
+
+var asciiLowercaseArray=[];
+    for(var i=97; i<123; i++) {
+        asciiLowercaseArray.push(i);
+    } 
+    console.log(asciiLowercaseArray); 
+
+//generate ascii value
+var lowercase = asciiLowercaseArray.map((i)=>String.fromCharCode(i)); 
+console.log(lowercase); 
+
+
+
+var asciiUpperArray=[];
+    for (var i=65; i<91; i++) {
+        asciiUpperArray.push(i);
+    }
+    console.log(asciiUpperArray);
+
+var uppercase = asciiUpperArray.map((i)=>String.fromCharCode(i));
+
+console.log(uppercase);
+
+var asciiNumberArray = []
+    for (var i=48; i<58; i++) {
+        asciiNumberArray.push(i)
+    }
+    console.log(asciiNumberArray);
+
+var numbers = asciiNumberArray.map((i)=>String.fromCharCode(i));
+console.log(numbers)
+
+//Special Chars, created 4 different arrays and concated them, would've been easier to type them out tbh
 var specialChars = []
+var asciiSpecialCharsArray1=[]
+    for(var i=33;i<48;i++) {
+        asciiSpecialCharsArray1.push(i);
+    }
+    console.log(asciiSpecialCharsArray1);
+
+var asciiSpecialCharsArray2=[]
+    for(var i=58;i<65;i++) {
+        asciiSpecialCharsArray2.push(i);
+    }
+    console.log(asciiSpecialCharsArray2);
+
+var asciiSpecialCharsArray3=[]
+    for(var i=91; i<97; i++) {
+    asciiSpecialCharsArray3.push(i);
+    }
+    console.log(asciiSpecialCharsArray3);
+
+var asciiSpecialCharsArray4=[]
+    for(var i=123; i<127; i++) {
+    asciiSpecialCharsArray3.push(i);
+    }
+    console.log(asciiSpecialCharsArray4);
+
+var asciiSpecialCharsArray = asciiSpecialCharsArray1.concat(
+    asciiSpecialCharsArray2.concat(
+        asciiSpecialCharsArray3.concat(
+            asciiSpecialCharsArray4)));
+            console.log(asciiSpecialCharsArray);
+
+/*Special Character Array - "((32) ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'])" - from console.log 
+
+ascii table - https://www.w3schools.com/charsets/ref_html_ascii.asp
+
+I must admit, this was not worth it. */
+
+var specialChars = asciiSpecialCharsArray.map((i)=>String.fromCharCode(i));
+
+console.log(specialChars);
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -20,34 +98,38 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // PHASE 1 
-// Create the variables and get the info we need
+// Create the variables and get the info we need (Lines 1-81)
+
 // What do we need to do? 
 var availableChars = []
-  //must PROMPT use for length of word (8-128)(use PROMPT)
-  //confirm user for upper/lower case (yes no?) - use CONFIRM
-    //if yes, add 'lowercase'
-    //if yes, add 'uppercase'
-  //confirm user if they'd like to use numbers
-    //if yes, add 'numbers'
-  //confirm user if they'd like to use special characters
-    //if yes, add 'special characters'
-  //Create a pool of available characters
-    //available chars are added if user confirms 'YES'
-    //available chars are NOT added if user confirms 'NO'
+    //must PROMPT use for length of word (8-128)(use PROMPT)
+    prompt("Enter password character length (must be 8-128 characters)")
+    //confirm user for upper/lower case (yes no?) - use CONFIRM
+        //if yes, add 'lowercase'
+        //if yes, add 'uppercase'
+    
+    //confirm user if they'd like to use numbers
+        
+    //confirm user if they'd like to use special characters
+        //if yes, add 'special characters'
+    //Create a pool of available characters
+        //available chars are added if user confirms 'YES'
+        //available chars are NOT added if user confirms 'NO'
 
-      /*Edge Cases - 
-        charcter length less than 8 characters
-        character length longer than 128 characters
-        user does not confirm "YES" to pool of available characters
-        user types out numbers... ex. "twelve" */
+          /*Edge Cases - 
+            charcter length less than 8 characters
+            character length longer than 128 characters
+            user does not confirm "YES" to pool of available characters
+            user types out numbers... ex. "twelve" */
 
 // PHASE 2
 //What values do we need?
-  //Create password variable that is an empty string
-  //create a 'for' loop that runs '.length' number of times
-  //in for loop, generate a single random character and append this character to our password
-    //FOR LOOP STEPS
-    //create a random number between 0 and length consisting of our available pool character types array depending on user selection
-    //select 1 element from array of availablechars at index random number we generate
-    //var newChar = avilablechars[randomNum]
-    //append this new element to password variable
+    //Create password variable that is an empty string
+    //create a 'for' loop that runs '.length' number of times
+    //in for loop, generate a single random character and append this character to our password
+        //FOR LOOP STEPS
+            //create a random number between 0 and length consisting of our available pool character types array depending on user selection
+            //select 1 element from array of availablechars at index random number we generate
+            //var newChar = avilablechars[randomNum]
+            //append this new element to password variable
+
